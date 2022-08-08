00ac9dd22171ff0687022c9f1e43ffda1367a0a9
Николай Гучок <nikolajgucok@MacBook-Pro-Nikolaj.local>
2022-08-08 22:40:21 +0300

    bracketss

--- src/index.js
+++ src/index.js
@@ -1,3 +1,22 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
-}
+  const openBrackets = [];
+  const closeBrackets = [];
+  bracketsConfig.forEach((item, index) => {
+    openBrackets[index] = item[0];
+    closeBrackets[index] = item[1];
+  });
+  let check = [];
+  for (let i = 0; i < str.length; i++) {
+    if (
+      closeBrackets.indexOf(str[i]) !== -1 &&
+      check[check.length - 1] === openBrackets[closeBrackets.indexOf(str[i])]
+    ) {
+      check.pop(closeBrackets[closeBrackets.indexOf(str[i])]);
+    } else if (openBrackets.indexOf(str[i]) !== -1) {
+      check.push(openBrackets[openBrackets.indexOf(str[i])]);
+    } else {
+      return false;
+    }
+  }
+  return check.length === 0;
+};
